d1b6bbc00cf6b7031893a2c8e097d4cb97968a6b
Lyubov <n.lubovj@gmail.com>
2023-07-25 23:54:23 +0300

    Add DownloadServlet class

---
+++ src/main/java/ru/job4j/servlet/DownloadServlet.java
@@ -0,0 +1,17 @@
+package ru.job4j.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+
+public class DownloadServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        for (File file : new File("c:\\images\\").listFiles()) {
+            System.out.println(file.getAbsolutePath());
+        }
+    }
+}
