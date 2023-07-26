84ba19f0e13708e995203291bd6dcd7a688d11d9
Lyubov <n.lubovj@gmail.com>
2023-07-26 23:54:51 +0300

    Correct DownloadServlet class

--- src/main/java/ru/job4j/servlet/DownloadServlet.java
+++ src/main/java/ru/job4j/servlet/DownloadServlet.java
@@ -5,13 +5,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class DownloadServlet extends HttpServlet {
+    private final static String REQUIRED_FILE_NAME = "users.txt";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        File users = null;
         for (File file : new File("c:\\images\\").listFiles()) {
-            System.out.println(file.getAbsolutePath());
+            if (REQUIRED_FILE_NAME.equals(file.getName())) {
+                users = file;
+                break;
+            }
+        }
+        try (FileInputStream stream = new FileInputStream(users)) {
+            resp.setContentType("application/octet-stream");
+            resp.setHeader("Content-Disposition", "attachment; filename=\"" + users.getName() + "\"");
+            resp.getOutputStream().write(stream.readAllBytes());
         }
     }
 }
