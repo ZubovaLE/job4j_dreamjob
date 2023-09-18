de915c01cc86e112d35dc383cabb0e6803730304
Lyubov <n.lubovj@gmail.com>
2023-09-18 23:51:44 +0300

    Improve project

--- src/main/java/ru/job4j/servlet/AuthServlet.java
+++ src/main/java/ru/job4j/servlet/AuthServlet.java
@@ -11,6 +11,11 @@ import java.io.IOException;
 
 public class AuthServlet extends HttpServlet {
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
